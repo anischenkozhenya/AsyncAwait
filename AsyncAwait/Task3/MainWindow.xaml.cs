0a961d85f42cfc08293ce1952738450221815b15
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-31 23:41:21 +0300

    +Task3

--- AsyncAwait/Task3/MainWindow.xaml.cs
+++ AsyncAwait/Task3/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 //Создайте WPF приложение, разместите в окне TextBox и две кнопки.При нажатии на первую
 //кнопку в TextBox выводится сообщение «Подключен к базе данных» при этом в обработчике
 //установите задержку в 3-5 сек для имитации подключения к БД, также данная кнопка запускает
@@ -25,9 +16,42 @@ namespace Task3
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        private void MyTick(object sender, EventArgs e)
+        {
+            TextboxMain.Text += "Данные получены\n";
+        }
+        static string ConnectMethod()
+        {
+            Thread.Sleep(5000);
+            return "Подключен к базе данных";
+        }
+        static string Disconnect()
+        {
+            return "Отключен от базы данных";
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += new EventHandler(MyTick);
         }
+
+        private async void ConnectToDataBase_Click(object sender, RoutedEventArgs e)
+        {
+            Task<string> task = Task.Factory.StartNew(new Func<string>(ConnectMethod));
+            TextboxMain.Text += await task + "\n";
+            timer.Start();
+        }
+
+        private async void ConnectToDataBase_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            Task<string> task = new Task<string>(Disconnect);
+            task.Start();
+            TextboxMain.Text = TextboxMain.Text + await task + "\n";
+        }
+
     }
 }
