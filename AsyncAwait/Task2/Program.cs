5f751ade1942802c088da8893f9f5429d1410a82
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-31 23:30:12 +0300

    +task2

--- AsyncAwait/Task2/Program.cs
+++ AsyncAwait/Task2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 //Используя AsyncAwait, создайте метод, который будет в цикле for (допустим, на 10
 //итераций) увеличивать счетчик на единицу и выводить на экран счетчик и текущий поток.
@@ -12,10 +13,29 @@ namespace Task2
 {
     class Program
     {
-
+        static int counter=0;
+        static private object obj = new object();
+        static private async Task Method()
+        {
+            await Task.Run(() =>
+            {
+                lock (obj)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine("{0} в потоке {1}", ++counter,Thread.CurrentThread.ManagedThreadId.ToString());
+                    }
+                }
+            });
+        }
         static void Main(string[] args)
         {
-
+            Task task1 = Method();
+            Task task2 = Method();
+            Task task3 = Method();
+            Task.WaitAll(task1,task2,task3);
+            Console.WriteLine("Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
         }
     }
 }
